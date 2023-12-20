2687d021c0ced5e07a1bed74591406d10a11899d
ayushi-joshi <ayushij103@gmail.com>
2023-12-20 11:21:34 +0530

    change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,6 +23,27 @@ const Home = () => {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  
   };
 
 
